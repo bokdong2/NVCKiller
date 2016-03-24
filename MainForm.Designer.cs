f5b6c0785a10cfc1767f94364eb7781263d3193c
PhilipMo <nodezero@naver.com>
2016-03-24 15:14:28 +0900

    Naver Vaccine Kill
    Naver Vaccine Kill with self-elevate ( Using administrator Privileges )

---
+++ MainForm.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace CSUACSelfElevation
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnElevate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbInAdminGroup = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbIsRunAsAdmin = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbIsElevated = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lbIntegrityLevel = new System.Windows.Forms.Label();
+            this.btnKill = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnElevate
+            // 
+            this.btnElevate.Location = new System.Drawing.Point(19, 108);
+            this.btnElevate.Name = "btnElevate";
+            this.btnElevate.Size = new System.Drawing.Size(211, 24);
+            this.btnElevate.TabIndex = 0;
+            this.btnElevate.Text = "Bypass Protection";
+            this.btnElevate.UseVisualStyleBackColor = true;
+            this.btnElevate.Click += new System.EventHandler(this.btnElevate_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "IsUserInAdminGroup:";
+            // 
+            // lbInAdminGroup
+            // 
+            this.lbInAdminGroup.AutoSize = true;
+            this.lbInAdminGroup.Location = new System.Drawing.Point(163, 12);
+            this.lbInAdminGroup.Name = "lbInAdminGroup";
+            this.lbInAdminGroup.Size = new System.Drawing.Size(0, 12);
+            this.lbInAdminGroup.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "IsRunAsAdmin:";
+            // 
+            // lbIsRunAsAdmin
+            // 
+            this.lbIsRunAsAdmin.AutoSize = true;
+            this.lbIsRunAsAdmin.Location = new System.Drawing.Point(163, 35);
+            this.lbIsRunAsAdmin.Name = "lbIsRunAsAdmin";
+            this.lbIsRunAsAdmin.Size = new System.Drawing.Size(0, 12);
+            this.lbIsRunAsAdmin.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 58);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "IsProcessElevated:";
+            // 
+            // lbIsElevated
+            // 
+            this.lbIsElevated.AutoSize = true;
+            this.lbIsElevated.Location = new System.Drawing.Point(163, 58);
+            this.lbIsElevated.Name = "lbIsElevated";
+            this.lbIsElevated.Size = new System.Drawing.Size(0, 12);
+            this.lbIsElevated.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 81);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 12);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Integrity Level:";
+            // 
+            // lbIntegrityLevel
+            // 
+            this.lbIntegrityLevel.AutoSize = true;
+            this.lbIntegrityLevel.Location = new System.Drawing.Point(163, 81);
+            this.lbIntegrityLevel.Name = "lbIntegrityLevel";
+            this.lbIntegrityLevel.Size = new System.Drawing.Size(0, 12);
+            this.lbIntegrityLevel.TabIndex = 8;
+            // 
+            // btnKill
+            // 
+            this.btnKill.Location = new System.Drawing.Point(19, 138);
+            this.btnKill.Name = "btnKill";
+            this.btnKill.Size = new System.Drawing.Size(211, 27);
+            this.btnKill.TabIndex = 9;
+            this.btnKill.Text = "Kill";
+            this.btnKill.UseVisualStyleBackColor = true;
+            this.btnKill.Click += new System.EventHandler(this.btnKill_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(250, 174);
+            this.Controls.Add(this.btnKill);
+            this.Controls.Add(this.lbIntegrityLevel);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lbIsElevated);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lbIsRunAsAdmin);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbInAdminGroup);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnElevate);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "NVCKiller";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnElevate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbInAdminGroup;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbIsRunAsAdmin;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbIsElevated;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lbIntegrityLevel;
+        private System.Windows.Forms.Button btnKill;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,405 @@
+﻿
+#region Using directives
+using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
+using System.Windows.Forms;
+#endregion
+
+
+namespace CSUACSelfElevation
+{
+    public partial class MainForm : Form
+    {
+        #region Helper Functions for Admin Privileges and Elevation Status
+        internal bool IsUserInAdminGroup()
+        {
+            bool fInAdminGroup = false;
+            SafeTokenHandle hToken = null;
+            SafeTokenHandle hTokenToCheck = null;
+            IntPtr pElevationType = IntPtr.Zero;
+            IntPtr pLinkedToken = IntPtr.Zero;
+            int cbSize = 0;
+
+            try
+            {
+
+                if (!NativeMethods.OpenProcessToken(Process.GetCurrentProcess().Handle,
+                    NativeMethods.TOKEN_QUERY | NativeMethods.TOKEN_DUPLICATE, out hToken))
+                {
+                    throw new Win32Exception();
+                }
+
+
+                if (Environment.OSVersion.Version.Major >= 6)
+                {
+
+                    cbSize = sizeof(TOKEN_ELEVATION_TYPE);
+                    pElevationType = Marshal.AllocHGlobal(cbSize);
+                    if (pElevationType == IntPtr.Zero)
+                    {
+                        throw new Win32Exception();
+                    }
+
+   
+                    if (!NativeMethods.GetTokenInformation(hToken, 
+                        TOKEN_INFORMATION_CLASS.TokenElevationType, pElevationType,
+                        cbSize, out cbSize))
+                    {
+                        throw new Win32Exception();
+                    }
+
+                    TOKEN_ELEVATION_TYPE elevType = (TOKEN_ELEVATION_TYPE)
+                        Marshal.ReadInt32(pElevationType);
+
+                    if (elevType == TOKEN_ELEVATION_TYPE.TokenElevationTypeLimited)
+                    { 
+                        cbSize = IntPtr.Size;
+                        pLinkedToken = Marshal.AllocHGlobal(cbSize);
+                        if (pLinkedToken == IntPtr.Zero)
+                        {
+                            throw new Win32Exception();
+                        }
+
+ 
+                        if (!NativeMethods.GetTokenInformation(hToken,
+                            TOKEN_INFORMATION_CLASS.TokenLinkedToken, pLinkedToken,
+                            cbSize, out cbSize))
+                        {
+                            throw new Win32Exception();
+                        }
+
+                        IntPtr hLinkedToken = Marshal.ReadIntPtr(pLinkedToken);
+                        hTokenToCheck = new SafeTokenHandle(hLinkedToken);
+                    }
+                }
+                
+
+                if (hTokenToCheck == null)
+                {
+                    if (!NativeMethods.DuplicateToken(hToken,
+                        SECURITY_IMPERSONATION_LEVEL.SecurityIdentification,
+                        out hTokenToCheck))
+                    {
+                        throw new Win32Exception();
+                    }
+                }
+
+
+                WindowsIdentity id = new WindowsIdentity(hTokenToCheck.DangerousGetHandle());
+                WindowsPrincipal principal = new WindowsPrincipal(id);
+                fInAdminGroup = principal.IsInRole(WindowsBuiltInRole.Administrator);
+            }
+            finally
+            {
+                if (hToken != null)
+                {
+                    hToken.Close();
+                    hToken = null;
+                }
+                if (hTokenToCheck != null)
+                {
+                    hTokenToCheck.Close();
+                    hTokenToCheck = null;
+                }
+                if (pElevationType != IntPtr.Zero)
+                {
+                    Marshal.FreeHGlobal(pElevationType);
+                    pElevationType = IntPtr.Zero;
+                }
+                if (pLinkedToken != IntPtr.Zero)
+                {
+                    Marshal.FreeHGlobal(pLinkedToken);
+                    pLinkedToken = IntPtr.Zero;
+                }
+            }
+
+            return fInAdminGroup;
+        }
+
+
+
+        internal bool IsRunAsAdmin()
+        {
+            WindowsIdentity id = WindowsIdentity.GetCurrent();
+            WindowsPrincipal principal = new WindowsPrincipal(id);
+            return principal.IsInRole(WindowsBuiltInRole.Administrator);
+        }
+
+
+      
+        internal bool IsProcessElevated()
+        {
+            bool fIsElevated = false;
+            SafeTokenHandle hToken = null;
+            int cbTokenElevation = 0;
+            IntPtr pTokenElevation = IntPtr.Zero;
+
+            try
+            {
+
+                if (!NativeMethods.OpenProcessToken(Process.GetCurrentProcess().Handle,
+                    NativeMethods.TOKEN_QUERY, out hToken))
+                {
+                    throw new Win32Exception();
+                }
+
+                cbTokenElevation = Marshal.SizeOf(typeof(TOKEN_ELEVATION));
+                pTokenElevation = Marshal.AllocHGlobal(cbTokenElevation);
+                if (pTokenElevation == IntPtr.Zero)
+                {
+                    throw new Win32Exception();
+                }
+
+     
+                if (!NativeMethods.GetTokenInformation(hToken, 
+                    TOKEN_INFORMATION_CLASS.TokenElevation, pTokenElevation,
+                    cbTokenElevation, out cbTokenElevation))
+                {
+
+                    throw new Win32Exception();
+                }
+
+ 
+                TOKEN_ELEVATION elevation = (TOKEN_ELEVATION)Marshal.PtrToStructure(
+                    pTokenElevation, typeof(TOKEN_ELEVATION));
+
+                fIsElevated = (elevation.TokenIsElevated != 0);
+            }
+            finally
+            {
+                
+                if (hToken != null)
+                {
+                    hToken.Close();
+                    hToken = null;
+                }
+                if (pTokenElevation != IntPtr.Zero)
+                {
+                    Marshal.FreeHGlobal(pTokenElevation);
+                    pTokenElevation = IntPtr.Zero;
+                    cbTokenElevation = 0;
+                }
+            }
+
+            return fIsElevated;
+        }
+
+
+  
+        internal int GetProcessIntegrityLevel()
+        {
+            int IL = -1;
+            SafeTokenHandle hToken = null;
+            int cbTokenIL = 0;
+            IntPtr pTokenIL = IntPtr.Zero;
+
+            try
+            {
+        
+                if (!NativeMethods.OpenProcessToken(Process.GetCurrentProcess().Handle,
+                    NativeMethods.TOKEN_QUERY, out hToken))
+                {
+                    throw new Win32Exception();
+                }
+
+
+                if (!NativeMethods.GetTokenInformation(hToken,
+                    TOKEN_INFORMATION_CLASS.TokenIntegrityLevel, IntPtr.Zero, 0,
+                    out cbTokenIL))
+                {
+                    int error = Marshal.GetLastWin32Error();
+                    if (error != NativeMethods.ERROR_INSUFFICIENT_BUFFER)
+                    {
+
+                        throw new Win32Exception(error);
+                    }
+                }
+                
+                pTokenIL = Marshal.AllocHGlobal(cbTokenIL);
+                if (pTokenIL == IntPtr.Zero)
+                {
+                    throw new Win32Exception();
+                }
+
+
+                if (!NativeMethods.GetTokenInformation(hToken,
+                    TOKEN_INFORMATION_CLASS.TokenIntegrityLevel, pTokenIL, cbTokenIL,
+                    out cbTokenIL))
+                {
+                    throw new Win32Exception();
+                }
+
+                TOKEN_MANDATORY_LABEL tokenIL = (TOKEN_MANDATORY_LABEL)
+                    Marshal.PtrToStructure(pTokenIL, typeof(TOKEN_MANDATORY_LABEL));
+
+                IntPtr pIL = NativeMethods.GetSidSubAuthority(tokenIL.Label.Sid, 0);
+                IL = Marshal.ReadInt32(pIL);
+            }
+            finally
+            {
+
+                if (hToken != null)
+                {
+                    hToken.Close();
+                    hToken = null;
+                }
+                if (pTokenIL != IntPtr.Zero)
+                {
+                    Marshal.FreeHGlobal(pTokenIL);
+                    pTokenIL = IntPtr.Zero;
+                    cbTokenIL = 0;
+                }
+            }
+
+            return IL;
+        }
+
+        #endregion
+
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            Console.WriteLine("Module Loaded.");
+            try
+            {
+                bool fInAdminGroup = IsUserInAdminGroup();
+                this.lbInAdminGroup.Text = fInAdminGroup.ToString();
+            }
+            catch (Exception ex)
+            {
+                this.lbInAdminGroup.Text = "N/A";
+                MessageBox.Show(ex.Message, "An error occurred in IsUserInAdminGroup",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            try
+            {
+                bool fIsRunAsAdmin = IsRunAsAdmin();
+                this.lbIsRunAsAdmin.Text = fIsRunAsAdmin.ToString();
+            }
+            catch (Exception ex)
+            {
+                this.lbIsRunAsAdmin.Text = "N/A";
+                MessageBox.Show(ex.Message, "An error occurred in IsRunAsAdmin",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+
+         
+            if (Environment.OSVersion.Version.Major >= 6)
+            {
+
+                try
+                {
+                   
+                    bool fIsElevated = IsProcessElevated();
+                    this.lbIsElevated.Text = fIsElevated.ToString();
+
+
+                    this.btnElevate.FlatStyle = FlatStyle.System;
+                    NativeMethods.SendMessage(btnElevate.Handle,
+                        NativeMethods.BCM_SETSHIELD, 0,
+                        fIsElevated ? IntPtr.Zero : (IntPtr)1);
+                }
+                catch (Exception ex)
+                {
+                    this.lbIsElevated.Text = "N/A";
+                    MessageBox.Show(ex.Message, "An error occurred in IsProcessElevated",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+                try
+                {
+
+                    int IL = GetProcessIntegrityLevel();
+                    switch (IL)
+                    {
+                        case NativeMethods.SECURITY_MANDATORY_UNTRUSTED_RID:
+                            this.lbIntegrityLevel.Text = "Untrusted"; break;
+                        case NativeMethods.SECURITY_MANDATORY_LOW_RID:
+                            this.lbIntegrityLevel.Text = "Low"; break;
+                        case NativeMethods.SECURITY_MANDATORY_MEDIUM_RID:
+                            this.lbIntegrityLevel.Text = "Medium"; break;
+                        case NativeMethods.SECURITY_MANDATORY_HIGH_RID:
+                            this.lbIntegrityLevel.Text = "High"; break;
+                        case NativeMethods.SECURITY_MANDATORY_SYSTEM_RID:
+                            this.lbIntegrityLevel.Text = "System"; break;
+                        default:
+                            this.lbIntegrityLevel.Text = "Unknown"; break;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    this.lbIntegrityLevel.Text = "N/A";
+                    MessageBox.Show(ex.Message, "An error occurred in GetProcessIntegrityLevel",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                this.lbIsElevated.Text = "N/A";
+                this.lbIntegrityLevel.Text = "N/A";
+            }
+        }
+
+
+        private void btnElevate_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Execute Self-Elevate");
+            if (!IsRunAsAdmin())
+            {
+
+                ProcessStartInfo proc = new ProcessStartInfo();
+                proc.UseShellExecute = true;
+                proc.WorkingDirectory = Environment.CurrentDirectory;
+                proc.FileName = Application.ExecutablePath;
+                proc.Verb = "runas";
+
+                try
+                {
+                    Process.Start(proc);
+                }
+                catch
+                {
+
+                    return;
+                }
+
+                Application.Exit();
+            }
+            else
+            {
+                MessageBox.Show("The process is running as administrator", "UAC");
+            }
+        }
+
+        private void btnKill_Click(object sender, EventArgs e)
+        {
+            Process[] processList = Process.GetProcesses();
+
+            try {
+                foreach (Process nvc in processList)
+                {
+                    if (nvc.ProcessName.ToLower() == "nvcagent.npc" || nvc.ProcessName.ToLower() == "nsavsvc.npc" || nvc.ProcessName.ToLower() == "nsvmon.npc")
+                    {  
+                        nvc.Kill();
+                        Console.WriteLine(nvc.ProcessName.ToLower() + " Killed By NVCKiller");
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Access Denind");
+            }   
+        }
+    }
+}
